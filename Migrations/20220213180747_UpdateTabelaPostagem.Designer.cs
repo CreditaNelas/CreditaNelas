dd730fd3ff638825d5369ee80c2cc00dccf3e1e2
Felipe Rodrigues <68449279+liperodrigues82@users.noreply.github.com>
2022-02-13 16:54:55 -0300

    nova tabela postagem, com url para imagem + atualização das views referentes

--- Migrations/20220213180747_UpdateTabelaPostagem.Designer.cs
+++ Migrations/20220213180747_UpdateTabelaPostagem.Designer.cs
@@ -4,14 +4,16 @@ using CreditaNelas.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CreditaNelas.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20220213180747_UpdateTabelaPostagem")]
+    partial class UpdateTabelaPostagem
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
